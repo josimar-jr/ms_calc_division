b29cedc6f3578cab4d9a04d4e0b6b50daa64532b
josimar-jr <shikamarujunior@gmail.com>
2020-11-13 00:38:22 -0300

    cria a pasta Division para conter a solução de produção da aplicação

--- Division/Services/HealthcheckService.cs
+++ Division/Services/HealthcheckService.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace division.Services
 {
